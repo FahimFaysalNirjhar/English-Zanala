2d7669fb7871f744c4fb7338ef2695a734570b76
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-01-14 01:46:35 +0600

    js fixed

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const main = document.querySelector("main");
+const heading = document.querySelector("#heading");
+const textBlue = document.getElementById("text-blue");
+const para = document.getElementById("para");
+const login = document.getElementById("login");
+
+main.classList.add("flex");
+main.classList.add("flex-col");
+main.classList.add("md:flex-row");
+main.classList.add("items-center");
+main.classList.add("justify-between");
+main.classList.add("w-11/12");
+main.classList.add("mt-20");
+main.classList.add("mx-auto");
+
+heading.classList.add("text-5xl");
+heading.classList.add("font-poppins");
+heading.classList.add("font-bold");
+
+textBlue.classList.add("text-[#48cae4]");
+
+para.classList.add("text-2xl");
+para.classList.add("text-[#6c757d]");
+para.classList.add("text-[#6c757d]");
+para.classList.add("max-w-2xl");
+para.classList.add("leading-relaxed");
+para.classList.add("mt-6");
+para.classList.add("font-tiro");
+
+login.classList.add("bg-[#422AD5]");
+login.classList.add("text-white");
--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 const faq = document.getElementById("faq");
 const facebook = document.getElementById("facebook");
 const youtube = document.getElementById("youtube");
+const github = document.getElementById("github");
 facebook.href = "https://www.facebook.com";
 facebook.target = "_blank";
 youtube.target = "_blank";
 youtube.href = "https://www.youtube.com/";
+github.href = "https://github.com/FahimFaysalNirjhar";
+github.target = "_blank";
 
 function removeActive() {
   const activebtn = document.querySelectorAll(".active");
