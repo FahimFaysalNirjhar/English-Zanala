ee41f93343e2b7eefee153848aa49225d423b47f
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-01-14 01:23:15 +0600

    no footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
 const faq = document.getElementById("faq");
+const facebook = document.getElementById("facebook");
+const youtube = document.getElementById("youtube");
+facebook.href = "https://www.facebook.com";
+facebook.target = "_blank";
+youtube.target = "_blank";
+youtube.href = "https://www.youtube.com/";
 
 function removeActive() {
   const activebtn = document.querySelectorAll(".active");
